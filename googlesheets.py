f34eefa6ac5cff9600647f7d26aca208f4acaa35
Lin1ey-Laptop <nguyen.numbers@gmail.com>
2022-06-07 08:39:41 -0400

    tested google sheet appending

--- googlesheets.py
+++ googlesheets.py
@@ -1,12 +1,6 @@
 from __future__ import print_function
-
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from numpy import append
 import google_secrets
 
 from google.oauth2 import service_account
@@ -23,6 +17,20 @@ creds = service_account.Credentials.from_service_account_file(
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = google_secrets.PROJECT_ID
 
+def appendToSpreadsheet(data, spreadsheetId, spreadsheetName, spreadsheet):
+    """
+    given some data and the spreadsheet, append data to the spreadsheet
+    
+    this will write to the first empty row, keeping the same number of empty rows
+
+    :param data: an array of arrays of data
+    :param spreadsheetId: the id of the spreadsheet to append to
+    :param spreadsheetName: the name of the spreadsheet to append to
+    :param spreadsheet: the googlesheets called from .spreadsheets()
+    """
+    sheetRange = "{}!A1:D1".format(spreadsheetName)
+    spreadsheet.values().append(spreadsheetId=spreadsheetId, range=sheetRange,
+                                valueInputOption="USER_ENTERED", insertDataOption="INSERT_ROWS", body={"values": data}).execute()
 
 def main():
     """Shows basic usage of the Sheets API.
@@ -33,11 +41,13 @@ def main():
 
     # Call the Sheets API
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range="Inventory!A1:D1").execute()
-    values = result.get('values', [])
-    print(result)
-
-
+    # result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    #                             range="Inventory!A1:D3").execute()
+    # values = result.get('values', [])
+    # print(values)
+    data = [[1, 2, 3, 4], [5, 6, 7, 8]]
+    # sheet.values().append(spreadsheetId=SAMPLE_SPREADSHEET_ID, range="Inventory!A1:G1",
+    #                       valueInputOption="USER_ENTERED", insertDataOption="INSERT_ROWS", body={"values": data}).execute()
+    appendToSpreadsheet(data, SAMPLE_SPREADSHEET_ID, "Inventory", sheet)
 if __name__ == '__main__':
     main()
--- parsing_json.py
+++ parsing_json.py
@@ -1,12 +1,18 @@
 import json
 brickset = "brickset.json"
 
-bricksetFile = open(brickset)
-x = json.load(bricksetFile)
+def parseBricksetFile(fileName):
+    bricksetFile = open(fileName)
+    x = json.load(bricksetFile)
 
-for i in x['sets']:
-    print("Name: " + i['name'])
-    print("First Available US Date: " + i['LEGOCom']['US']['dateFirstAvailable'][0:10])
-    print("Theme: " + i['theme'])
-    print("Set Number " + str(i['number']))
-    print('\n')
+    array=[]
+
+    for i in x['sets']:
+        object = [i['name'], i['LEGOCom']['US']['dateFirstAvailable'][0:10], i['theme'], (i['number'])]
+        # print("Name: " + i['name'])
+        # print("First Available US Date: " + i['LEGOCom']['US']['dateFirstAvailable'][0:10])
+        # print("Theme: " + i['theme'])
+        # print("Set Number " + str(i['number']))
+        # print('\n')
+        array.append(object)
+    print(array)
